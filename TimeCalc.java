00c8ec8d6e2eb931bfe50bf7cefb9e0944fb7fe3
Dana Maimon <danamaimon@Mac.lan>
2025-11-12 14:46:11 +0200

    done

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,36 @@
 public class TimeCalc {
-    public static void main(String[] args) {
-        // Replace this comment with your code
+    public static void main(String[] args)
+     {
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int mToAdd = Integer.parseInt(args[1]);
+        int totalM = (hours * 60) + minutes + mToAdd;
+        int totalH = totalM / 60;
+        int newHour = totalH % 24;
+        int newMinutes = totalM - (totalH*60);
+        if (newHour<10)
+        {
+            System.out.print("0" + newHour + ":");
+        }
+        else if (newHour == 0) 
+        {
+            System.out.print("00:");
+        }
+        else
+        {
+            System.out.print(newHour + ":");
+        }
+        if (newMinutes<10)
+        {
+            System.out.println("0" + newMinutes);
+        }
+        else if (newMinutes == 0) 
+        {
+            System.out.println("00");
+        }
+        else
+        {
+            System.out.println(newMinutes);
+        }
     }
 }
